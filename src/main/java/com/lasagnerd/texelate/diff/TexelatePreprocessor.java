5fb246ee12acebe9f7737584c9bc5a13e0175faf
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2023-08-13 00:35:01 +0200

    Add support for variable expansion

--- src/main/java/com/lasagnerd/texelate/diff/TexelatePreprocessor.java
+++ src/main/java/com/lasagnerd/texelate/diff/TexelatePreprocessor.java
@@ -4,21 +4,18 @@ import com.intellij.lang.Language;
 import com.intellij.lang.properties.psi.Property;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiErrorElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
 import com.lasagnerd.texelate.completion.PropertiesUtils;
 import com.lasagnerd.texelate.ifblocks.PreprocessorFile;
 import com.lasagnerd.texelate.ifblocks.psi.PreprocessorEvaluableBlock;
-import com.lasagnerd.texelate.injection.IfStatementTokenPatterns;
-import com.lasagnerd.texelate.injection.IfStatementTokenPatterns.IfStatement;
 import com.lasagnerd.texelate.microbool.MicroboolLanguage;
 import com.lasagnerd.texelate.microbool.psi.MicroboolExpression;
 import com.lasagnerd.texelate.microbool.psi.MicroboolFile;
-import com.lasagnerd.texelate.ifblocks.psi.PreprocessorIfBlock;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 import static com.intellij.lang.Language.findLanguageByID;
 import static com.lasagnerd.texelate.microbool.psi.MicroboolPsiUtil.asBoolean;
@@ -37,7 +34,7 @@ public class TexelatePreprocessor {
         }
     }
 
-    public static boolean interpret(Map<String, Object> variables, MicroboolFile microboolFile) {
+    public static boolean evaluate(Map<String, Object> variables, MicroboolFile microboolFile) {
         if (microboolFile.getFirstChild() instanceof MicroboolExpression microboolExpression) {
             try {
                 return asBoolean(microboolExpression.evaluate(variables));
@@ -48,6 +45,100 @@ public class TexelatePreprocessor {
         return false;
     }
 
+    public static Object interpretValue(String value) {
+
+        if (value.toLowerCase(Locale.ROOT).equals("true")) {
+            return true;
+        }
+
+        if (value.toLowerCase(Locale.ROOT).equals("false")) {
+            return false;
+        }
+
+        return value;
+    }
+
+    @NotNull
+    private static String interpolateVariable(String variableName, Object value, String currentText) {
+        // escape variableName for regex
+        String valueString = value.toString();
+        String replacement = valueString.replaceAll("\\\\", "\\\\\\\\")
+                .replaceAll("\\$", "\\\\\\$");
+
+        currentText = currentText.replaceAll("\\$\\{%s}".formatted(variableName), replacement);
+
+        currentText = currentText
+                .replaceAll("\\$%s".formatted(variableName), replacement);
+        return currentText;
+    }
+
+    public static boolean evaluate(Project project, Map<String, Object> variables, String expression) {
+        PsiFile microboolPsiFile = PsiFileFactory.getInstance(project).createFileFromText(MicroboolLanguage.INSTANCE, expression);
+        if (microboolPsiFile instanceof MicroboolFile microboolFile) {
+            return TexelatePreprocessor.evaluate(variables, microboolFile);
+        } else {
+            throw new IllegalStateException(String.format("'%s' in if statement valid Microbool Expression", expression));
+        }
+    }
+    private static class VariablesExpander {
+        Map<String, Object> expandedVariables = new HashMap<>();
+        final Map<String, Object> variables;
+
+        public static Map<String, Object> expandVariables(Map<String, Object> variables) {
+            VariablesExpander variablesExpander = new VariablesExpander(variables);
+            variablesExpander.expandVariables();
+            return variablesExpander.expandedVariables;
+        }
+
+        private VariablesExpander(Map<String, Object> variables) {
+            this.variables = variables;
+        }
+
+        public void expandVariables() {
+            for (Map.Entry<String, Object> entry : variables.entrySet()) {
+                expandVariable(entry.getKey(), new HashSet<>());
+            }
+        }
+
+        public void expandVariable(String name, Set<String> visited) {
+            if(expandedVariables.containsKey(name)) {
+                return;
+            }
+
+            if(visited.contains(name)) {
+                throw new RuntimeException("Circular dependency detected");
+            }
+
+            visited.add(name);
+
+            Object value = variables.get(name);
+            if(value == null) {
+                expandedVariables.put(name, "");
+                return;
+            }
+
+            String valueString = value.toString();
+            List<String> referencedVariables = extractReferencedVariables(value.toString());
+            for (String referencedVariable : referencedVariables) {
+                expandVariable(referencedVariable, visited);
+                valueString = interpolateVariable(referencedVariable, expandedVariables.get(referencedVariable), valueString);
+            }
+            expandedVariables.put(name, interpretValue(valueString));
+        }
+
+        public static List<String> extractReferencedVariables(String expression) {
+            // Find all expressions in the form of ${variable}
+            List<String> variables = new ArrayList<>();
+
+            Pattern pattern = Pattern.compile("\\$\\{([^}]+)}");
+            pattern.matcher(expression).results().forEach(matchResult -> {
+                String variableName = matchResult.group(1);
+                variables.add(variableName);
+            });
+
+            return variables;
+        }
+    }
 
     private static class Interpreter {
         private static String interpret(String environment, PreprocessorFile psiFile) {
@@ -61,11 +152,9 @@ public class TexelatePreprocessor {
                 }
             }
 
-
-            return replaceVariables(text, interpreter.variables);
+            return replaceVariables(text, VariablesExpander.expandVariables(interpreter.variables));
         }
 
-        StringBuilder text = new StringBuilder();
         private final String environment;
         private final @NotNull Project project;
         Map<String, Object> variables = new HashMap<>();
@@ -88,77 +177,17 @@ public class TexelatePreprocessor {
             }
         }
 
-        private Object interpretValue(String value) {
 
-            if (value.toLowerCase(Locale.ROOT).equals("true")) {
-                return true;
-            }
-
-            if (value.toLowerCase(Locale.ROOT).equals("false")) {
-                return false;
-            }
-
-            return value;
-        }
-
-        private void processChild(PsiElement psiElement) {
-            if (psiElement instanceof PsiErrorElement) {
-                throw new RuntimeException("Preprocessor code is not well-formed!");
-            }
-
-            if (psiElement instanceof PreprocessorIfBlock preprocessorIfBlock) {
-                String openingIfStatementText = "";
-
-                Optional<IfStatement> ifStatementOptional = IfStatementTokenPatterns.parseOpeningIfStatement(openingIfStatementText);
-                if (ifStatementOptional.isPresent()) {
-                    IfStatement ifStatement = ifStatementOptional.get();
-                    String expression = ifStatement.expression().trim();
-
-                    PsiFile microboolPsiFile = PsiFileFactory.getInstance(project).createFileFromText(MicroboolLanguage.INSTANCE, expression);
-                    if (microboolPsiFile instanceof MicroboolFile microboolFile) {
-                        boolean interpretedValue = TexelatePreprocessor.interpret(variables, microboolFile);
-                        if (interpretedValue) {
-                            for (PsiElement child : preprocessorIfBlock.getChildren()) {
-                                processChild(child);
-                            }
-                        }
-                    } else {
-                        throw new IllegalStateException(String.format("'%s' in if statement valid Microbool Expression", expression));
-                    }
-                }
-            } else {
-                text.append(psiElement.getText());
-            }
-        }
 
         private static String replaceVariables(StringBuilder text, Map<String, Object> variables) {
             String currentText = text.toString();
 
             // Replaces strings in the for $variable or ${variable} with the value of the variable
             for (Map.Entry<String, Object> variable : variables.entrySet()) {
-                String variableName = variable.getKey();
-                // escape variableName for regex
-                Object value = variable.getValue();
-                String valueString = value.toString();
-                String replacement = valueString.replaceAll("\\\\", "\\\\\\\\")
-                        .replaceAll("\\$", "\\\\\\$");
-
-                currentText = currentText.replaceAll("\\$\\{%s}".formatted(variableName), replacement);
-
-                currentText = currentText
-                        .replaceAll("\\$%s".formatted(variableName), replacement);
+                currentText = interpolateVariable(variable.getKey(), variable.getValue(), currentText);
             }
 
             return currentText;
-        }
-    }
-
-    public static boolean evaluate(Project project, Map<String, Object> variables, String expression) {
-        PsiFile microboolPsiFile = PsiFileFactory.getInstance(project).createFileFromText(MicroboolLanguage.INSTANCE, expression);
-        if (microboolPsiFile instanceof MicroboolFile microboolFile) {
-            return TexelatePreprocessor.interpret(variables, microboolFile);
-        } else {
-            throw new IllegalStateException(String.format("'%s' in if statement valid Microbool Expression", expression));
         }
     }
 }
